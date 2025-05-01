6f0b10647140d99ec9c51397bde91db68e0fe43b
akashsagar2805 <akashsagar5869@gmail.com>
2025-05-02 00:11:30 +0530

    added create project feature

--- resources/js/Pages/Projects/Index.jsx
+++ resources/js/Pages/Projects/Index.jsx
@@ -6,7 +6,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
 import { Head, Link, router } from '@inertiajs/react'
 import TableHeading from '@/Components/TableHeading'
 
-export default function Index({ auth, projects, queryParams = null }) {
+export default function Index({ auth, projects, queryParams = null, success }) {
   queryParams = queryParams || {}
   const searchFieldChanged = (name, value) => {
     if (value) {
@@ -42,18 +42,27 @@ export default function Index({ auth, projects, queryParams = null }) {
     <AuthenticatedLayout
       user={auth.user}
       header={
-        <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-          Projects
-        </h2>
+        <div className="flex justify-between items-center">
+          <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            Projects
+          </h2>
+          <Link
+            href={route('project.create')}
+            className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition all hover:bg-emerald-600"
+          >
+            Add New
+          </Link>
+        </div>
       }
     >
       <Head title="Dashboard" />
 
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+          {success && <div className="bg-emerald-500 py-2 px-4 text-white rounded">{success}</div>}
+
+          <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800 mt-6">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-              {/* <pre>{JSON.stringify(projects, undefined, 2)}</pre> */}
               <div className="overflow-auto">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                   <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
